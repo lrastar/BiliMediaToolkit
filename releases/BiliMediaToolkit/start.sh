71b067c4cd44c7a118cc3302e34b591105f9520a
lrastar <143856673+lrastar@users.noreply.github.com>
2026-05-18 00:38:37 +0800

    fix: remove releases directory from git

--- releases/BiliMediaToolkit/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "[INFO] Starting BiliMediaToolkit..."
-cd server
-echo "[INFO] Installing dependencies..."
-if [ ! -d "node_modules" ]; then npm install --no-audit --no-fund; fi
-echo "[INFO] Starting server..."
-node index.js
